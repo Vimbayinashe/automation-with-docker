436d8121364a5b3b8f3cd648691f06513e55770d
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-01-25 14:19:29 +0100

    Add Dockerfile

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,5 +3,10 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello There!");
+        try {
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
