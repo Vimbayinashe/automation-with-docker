afd4ea9d32dce4c8691f61c8d19ae3886380a855
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-01-25 14:32:32 +0100

    Add configuration for cli mvn dockerBuild

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,10 +3,5 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello There!");
-        try {
-            Thread.sleep(100000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
