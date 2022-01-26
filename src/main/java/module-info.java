d46e9a96a0dd321d709ecad814080b6af3847223
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-01-26 13:13:38 +0100

    Add module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,10 @@
-module $MODULE_NAME$ {
+module org.example.appserver {
+
 }
+
+/* path to App: org.example.appserver/org/example/App
+ *
+ * exports org.example;                 // all classes are open to public
+ * exports org.example to java.base;    //all classes in org.example are open ONLY to java.base
+ * requires java.sql;                   //our code requires code from java.sql when it is run
+ */
