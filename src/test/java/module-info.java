bb51fd3c47fcf5dc360203dcf0634bdb84da7c34
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-01-26 13:13:55 +0100

    Add module-info for tests

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -1,2 +1,8 @@
-module $MODULE_NAME$ {
+open module org.example.appserver {
+    requires org.junit.jupiter;
+    requires org.mockito;
+    requires net.bytebuddy; //Mockito requires this
+    requires org.assertj.core;
 }
+
+//open -> open up all files in this module for reflection. This is necessary for junit & mockito
